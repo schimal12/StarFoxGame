15848088b7df1cdf2c136c639b93e0aec287c0d2
schimal12@gmail.com <schimal12@gmail.com>
2016-05-13 18:10:10 -0500

    Initial Commit

---
+++ ProyectoFinal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProyectoFinal
+//
+//  Created by Sebastian Chimal on 13/05/16.
+//  Copyright © 2016 Sebastian Chimal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
